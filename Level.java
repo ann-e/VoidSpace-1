8dfc545ca91604503b63cc31283bb16a610ce325
Derick <derick.rodriguez1@upr.edu>
2016-04-26 13:26:32 -0400

    Create Level.java

---
+++ Level.java
@@ -0,0 +1,34 @@
+package rbadia.voidspace.main;
+
+public class Level extends GameLogic{      //could extend GameLogic? 
+
+	
+	public Level(GameScreen gameScreen) {
+		super(gameScreen); 
+		System.out.println();
+		
+	}
+	
+//	public Level(GameScreen gameScreen, GameLogic gameLogic) {
+//		
+//		this.gameScreen = gameScreen;
+//		this.gameLogic = gameLogic;
+//		
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
